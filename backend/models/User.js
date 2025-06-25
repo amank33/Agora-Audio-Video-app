f50d0c0fc4cf4f2cb166335fb011e37c9cb2573a
amank33 <mukeshrani.aman@gmail.com>
2025-06-25 13:41:49 +0530

    Update User.js

--- backend/models/User.js
+++ backend/models/User.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 const UserSchema = new mongoose.Schema({
   name: String,
   email: { type: String, unique: true },
+  gender: String,
+  phone: String,
+  dob: String, // Store as string in dd/mm/yyyy format
   password: String,
-  rtmToken: String
+  rtmToken: String,  
+  rtcToken: String
 });
 export default mongoose.model('User', UserSchema);
